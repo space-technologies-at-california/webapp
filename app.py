a792d36e2731f35f07bab92368ccb834cc909608
Michael-Tu <tugan@berkeley.edu>
2017-10-14 03:24:48 -0700

    major update 1. Remove “…” from team page title 2. Add advisor page skeleton 3. Add hyperlink to main page for footer logo 4. Add custom css for advisor modal effects 5. Added advisors’ bio, images, and updated on the website 6. Move configuration to separate config.py file

--- app.py
+++ app.py
@@ -1,48 +1,11 @@
 import os
-import glob
-import json
 from flask import Flask
 from flask import render_template
 from flask import url_for
 from flask import redirect
+from config import config
 
 app = Flask(__name__)
-
-def load_json(path, root="static/data/", subroot=""):
-	root += subroot
-	return json.loads(open(root + path).read())
-
-if "cache.json" in os.listdir("."):
-	config = load_json("cache.json")
-	print("cache loaded...")
-else:
-	# Cache configuration files
-	config = {
-		"navbar": load_json("navbar.json"),
-		"club": load_json("club.json"),
-		"home" : load_json("home-configuration.json"),
-		"project": { x.split("/")[-1].split(".")[0]  : load_json(x, root="", subroot="") for x in glob.glob("static/data/projects/*.json") },
-		"team": load_json("team.json"),
-		"sponsor": load_json("sponsor.json"),
-		"sponsor-confirmation": load_json("sponsor-confirmation.json"),
-		"icon": load_json("icon.json")
-	}
-
-	# Cache configuration files II
-	config["team"].update({
-		"member": [ load_json(x, root="", subroot="") for x in glob.glob("static/data/member/*.json")]
-	})
-	# config['team']['member'].sort(key=lambda x: x['profile-order'] if x['profile-order'] is not None else x["name"]) 
-	# sort profile order
-	config['team']['member'] = sorted([x for x in config['team']['member'] if x["profile-order"] is not None], key=lambda x: x["profile-order"]) + sorted([x for x in config['team']['member'] if x["profile-order"] is None], key=lambda x: x["name"])
-
-	config["sponsor"].update({
-		"partner-logo": { x.split(".")[0] : x for x in glob.glob("static/img/logo/partner/*")}
-	})
-	# save cache
-	with open("static/data/cache.json", "w") as f:
-		f.write(json.dumps(config, indent=2))
-
 
 
 ##################### Cross Page Functionalities #####################
@@ -77,6 +40,11 @@ def project(name="example"):
 @app.route("/aboutus/team")
 def team():
 	return render_template("team.html", config=config["team"])
+
+@app.route("/advisor")
+@app.route("/aboutus/advisor")
+def advisor():
+	return render_template("advisor.html", config=config["advisor"])
 
 @app.route("/sponsor")
 def sponsor():
---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+import glob
+import json
+def load_json(path, root="static/data/", subroot=""):
+	root += subroot
+	return json.loads(open(root + path).read())
+
+if "cache.json" in os.listdir("."):
+	config = load_json("cache.json")
+	print("cache loaded...")
+else:
+	# Cache configuration files
+	config = {
+		"navbar": load_json("navbar.json"),
+		"club": load_json("club.json"),
+		"home" : load_json("home-configuration.json"),
+		"project": { x.split("/")[-1].split(".")[0]  : load_json(x, root="", subroot="") for x in glob.glob("static/data/projects/*.json") },
+		"advisor": load_json("advisor.json"),
+		"team": load_json("team.json"),
+		"sponsor": load_json("sponsor.json"),
+		"sponsor-confirmation": load_json("sponsor-confirmation.json"),
+		"icon": load_json("icon.json")
+	}
+
+	# Cache configuration for Team
+	config["team"].update({
+		"member": [ load_json(x, root="", subroot="") for x in glob.glob("static/data/member/*.json")]
+	})
+	# sort profile order
+	config['team']['member'] = sorted([x for x in config['team']['member'] if x["profile-order"] is not None], key=lambda x: x["profile-order"]) + sorted([x for x in config['team']['member'] if x["profile-order"] is None], key=lambda x: x["name"])
+
+	# Cache configuration for Advisor
+	config["advisor"].update({
+		"advisor": [ load_json(x, root="", subroot="") for x in glob.glob("static/data/advisor/*.json")]
+	})
+	# sort profile order
+	config['advisor']['advisor'] = sorted([x for x in config['advisor']['advisor'] if x["profile-order"] is not None], key=lambda x: x["profile-order"]) + sorted([x for x in config['advisor']['advisor'] if x["profile-order"] is None], key=lambda x: x["name"])
+
+	# Cache configuration for Sponsor
+	config["sponsor"].update({
+		"partner-logo": { x.split(".")[0] : x for x in glob.glob("static/img/logo/partner/*")}
+	})
+
+	# Save cache
+	with open("static/data/cache.json", "w") as f:
+		f.write(json.dumps(config, indent=2))
