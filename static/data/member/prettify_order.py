371b4665d76335e6f1d70f375a4bfbeb90c62517
Michael-Tu <tugan@berkeley.edu>
2018-04-16 20:04:25 -0500

    extend prettify_order for other folders

--- static/data/member/prettify_order.py
+++ static/data/member/prettify_order.py
@@ -1,15 +1,23 @@
 import os
 import json
 import glob
+import argparse
 
-members = glob.glob("*.json")
-old = list(map(lambda x: (x, json.load(open(x))), members))
+parser = argparse.ArgumentParser()
+parser.add_argument("folder", type=str)
+args = parser.parse_args()
 
-new = sorted([(x, y) for x, y in old if y["profile-order"] is not None], key=lambda x: x[1]["profile-order"])
-new += sorted([(x, y) for x, y in old if y["profile-order"] is None], key=lambda x: x[1]["name"])
+members = glob.glob(args.folder + "/*.json")
 
-for i, n in enumerate(new):
-    n[1]["profile-order"] = i + 1
-    with open(n[0], "w") as f:
-        json.dump(n[1], f, indent=2)
-    print(i, n[0])
+print(members)
+
+# old = list(map(lambda x: (x, json.load(open(x))), members))
+
+# new = sorted([(x, y) for x, y in old if y["profile-order"] is not None], key=lambda x: x[1]["profile-order"])
+# new += sorted([(x, y) for x, y in old if y["profile-order"] is None], key=lambda x: x[1]["name"])
+
+# for i, n in enumerate(new):
+#     n[1]["profile-order"] = i + 1
+#     with open(n[0], "w") as f:
+#         json.dump(n[1], f, indent=2)
+#     print(i+1, n[0])
